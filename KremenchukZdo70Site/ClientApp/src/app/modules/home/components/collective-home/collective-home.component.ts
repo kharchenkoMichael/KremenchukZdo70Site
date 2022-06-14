beecec911cbc16b45459349702890d6e967b39e9
Mykhailo Kharchenko CW <kharchenko.michael.a@gmail.com>
2022-06-14 14:13:50 +0300

    Add frontend part employee service

--- KremenchukZdo70Site/ClientApp/src/app/modules/home/components/collective-home/collective-home.component.ts
+++ KremenchukZdo70Site/ClientApp/src/app/modules/home/components/collective-home/collective-home.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { EmployeeService } from '@base/services/web-api/employee/employee.service';
+import { CollectiveItemResponse } from '@shared/models/collective-item-response';
+import { CollectiveRequest } from '@shared/models/collective-request';
 
 @Component({
   selector: 'app-collective-home',
   templateUrl: './collective-home.component.html',
-  styleUrls: ['./collective-home.component.scss']
+  styleUrls: ['./collective-home.component.scss'],
 })
 export class CollectiveHomeComponent implements OnInit {
+  public collective: CollectiveItemResponse[] = [];
+  public currentPage: number = 0;
+  public allPageCount: number = 0;
+  // private readonly subscriptions = new Subscription();
+  // private searchSubject: Subject<PaginationData> =
+  // new Subject<PaginationData>();
 
-  constructor() { }
+  constructor(private readonly employeeService: EmployeeService) {}
 
   ngOnInit(): void {
+    var request = new CollectiveRequest();
+    request.calcCountTotal = true;
+    request.page = 0;
+    request.size = 6;
+    this.employeeService.getCollectiveAsync(request).subscribe((result) => {
+      this.collective = result.data;
+      this.allPageCount = Math.ceil(result.totalCount / 6);
+    });
   }
-
 }
